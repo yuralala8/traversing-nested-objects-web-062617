e7eee7d4511537985623988229c76ddc32e64f1f
Yura Choi <yc1435@nyu.edu>
2017-08-09 00:15:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "Awesome Name",
+  occupation: {
+    title: "Senior Manager of Awesome",
+    yearsHeld: 2
+  },
+  pets: [{
+    kind: "dog",
+    name: "Fiona"
+  }, {
+    kind: "cat",
+    name: "Ralph"
+  }]
+}
